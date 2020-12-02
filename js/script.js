4375cc58081ff998da983e13ecb4e8a33aa17825
giancarlo zuliani <slaigox@gmail.com>
2020-12-02 16:55:56 +0100

    milestone 3 complete

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const APP = new Vue({
   data:{
     apiKey : '7a61773aa391abd450409993c20193bd',
     filmsArray : [],
+    seriesArray:[],
     searchinput:""
   },
   mounted : function(){
@@ -14,12 +15,16 @@ const APP = new Vue({
   },
   methods:{
     search(){
-      let x = this.searchinput.replace(/\s/g,'+');
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey + '&query=' + x)
-      .then(response => {
-        this.filmsArray = response.data.results;
-        console.log(response.data.results);
-      })
+
+      let films = 'https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey + '&query=' + this.searchinput.replace(/\s/g,'+');
+      let series = 'https://api.themoviedb.org/3/search/tv?api_key=' + this.apiKey  + '&query=' + this.searchinput.replace(/\s/g,'+');
+      let filmRequest = axios.get(films);
+      let seriesRequest = axios.get(series);
+      axios.all([filmRequest , seriesRequest])
+      .then(axios.spread((...responses) => {
+      this.filmsArray = responses[0].data.results;
+      this.seriesArray = responses[1].data.results;
+      }))
     }
   }
 })
