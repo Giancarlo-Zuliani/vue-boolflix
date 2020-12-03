03a61f8299442c841b2644ccb268388ac370e692
giancarlo zuliani <slaigox@gmail.com>
2020-12-03 11:02:46 +0100

    code clean

--- js/script.js
+++ js/script.js
@@ -2,31 +2,29 @@ const APP = new Vue({
   el:'#root',
   data:{
     apiKey : '7a61773aa391abd450409993c20193bd',
-    filmsArray : [],
-    seriesArray:[],
+    dataArray : [],
     searchinput:"",
     imgsrc:'https://image.tmdb.org/t/p/w342'
   },
   mounted : function(){
     axios.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&query=&api_key=' + this.apiKey )
     .then(response => {
-      this.filmsArray = response.data.results;
+      this.dataArray = response.data.results;
     });
     axios.get('https://api.themoviedb.org/3/discover/tv?sort_by=popularity.desc&query=&api_key=' + this.apiKey )
     .then(response=>{
-      this.seriesArray = response.data.results;
+      this.dataArray = [...this.dataArray , ...response.data.results]
     });
   },
   methods:{
     search(){
-      let films = 'https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey + '&query=' + this.searchinput.replace(/\s/g,'+');
+      let movies = 'https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey + '&query=' + this.searchinput.replace(/\s/g,'+');
       let series = 'https://api.themoviedb.org/3/search/tv?api_key=' + this.apiKey  + '&query=' + this.searchinput.replace(/\s/g,'+');
-      let filmRequest = axios.get(films);
+      let filmRequest = axios.get(movies);
       let seriesRequest = axios.get(series);
       axios.all([filmRequest , seriesRequest])
       .then(axios.spread((...responses) => {
-      this.filmsArray = responses[0].data.results;
-      this.seriesArray = responses[1].data.results;
+      this.dataArray = [...responses[0].data.results , ...responses[1].data.results];
     }));
     }
   }
